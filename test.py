961c7764c9ba0bcbdcea92deff3338fb56ee0429
RupertWieser <rupert.wieser@naotilus.de>
2023-04-14 17:52:23 +0200

    test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from pyconao.naopandas import NaoPandas
-
+from datetime import datetime
 
 Nao = NaoPandas(host="",email="",password="")
 
@@ -12,7 +12,14 @@ print(instances)
 series = Nao.getSeries()
 print(series)
 
-dataframe = Nao.getSeriesData(series[:350],interval="1d")
+dataframe = Nao.getSeriesData(
+    series=series[:350],
+    interval="1d",
+    aggregate="mean",
+    validate=True,
+    start=datetime.fromisoformat("2021-12-31 23:00:00"),
+    stop=datetime.fromisoformat("2022-01-30 23:00:00")
+)
 print(dataframe)
 
 
